2e3e6609aeb2d4e669f6b7047a6d62f70a42e8ff
pastorsj <pastorsj@rose-hulman.edu>
2017-06-09 09:27:09 -0400

    Updated to passport strategy:

--- server/app.js
+++ server/app.js
@@ -13,13 +13,31 @@ var cors = require('cors');
 
 var app = express();
 
+var passport = require('passport');
+var LinkedInStrategy = require('passport-linkedin-oauth2').Strategy;
+var session = require('express-session');
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(session(
+  {
+    secret: 'SECRET',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
+  }
+)); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('view engine', 'jade');
 
 app.use(cors({
     origin: '*',
@@ -31,7 +49,24 @@ app.use(cors({
 
 app.use('/', index);
 app.use('/users', users);
-app.use('/oauth', auth);
+app.use('/auth', auth);
+
+passport.use(new LinkedInStrategy({
+  clientID: process.env['LINKED_IN_APP_ID'],
+  clientSecret: process.env['LINKED_IN_SECRET'],
+  callbackURL: "http://127.0.0.1:3000/auth/linkedin/callback",
+  scope: ['r_emailaddress', 'r_basicprofile'],
+  state: true
+}, function(accessToken, refreshToken, profile, done) {
+  // asynchronous verification, for effect...
+  process.nextTick(function () {
+    // To keep the example simple, the user's LinkedIn profile is returned to
+    // represent the logged-in user. In a typical application, you would want
+    // to associate the LinkedIn account with a user record in your database,
+    // and return that user instead.
+    return done(null, profile);
+  });
+}));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,36 +1,16 @@
 var express = require('express');
 var router = express.Router();
 var Linkedin = require('node-linkedin')(process.env['LINKED_IN_APP_ID'], process.env['LINKED_IN_SECRET']);
+var passport = require('passport');
 
-Linkedin.auth.setCallback('callback');
-
-var scope = ['r_basicprofile', 'r_fullprofile', 'r_emailaddress', 'r_network', 'r_contactinfo', 'rw_nus', 'rw_groups', 'w_messages'];
-
-
-/* GET home page. */
-router.get('/linkedin', function(req, res) {
-    // This will ask for permisssions etc and redirect to callback url. 
-    Linkedin.auth.authorize(res, scope);
+router.get('/linkedin', passport.authenticate('linkedin'), function(req, res){
+    // The request will be redirected to LinkedIn for authentication, so this
+    // function will not be called.
 });
 
-router.get('/linkedin/callback', function(req, res) {
-    Linkedin.auth.getAccessToken(res, req.query.code, req.query.state, function(err, results) {
-        if ( err )
-            return console.error(err);
- 
-        /**
-         * Results have something like:
-         * {"expires_in":5184000,"access_token":". . . ."}
-         */
-
-        console.log(results);
-
-        var linkedin = Linkedin.init(results['access_token'], {
-            timeout: 10000 /* 10 seconds */
-        });
-
-        return res.redirect('/');
-    });
-});
+router.get('/linkedin/callback', passport.authenticate('linkedin', {
+  successRedirect: '/',
+  failureRedirect: '/login'
+}));
 
 module.exports = router;
